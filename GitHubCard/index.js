2bee20f33a7fd0e4433d4c31b77e780733f1feb6
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-04-09 20:16:38 -0400

    done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,14 +1,46 @@
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the following URL ///////////////////////////////////////DONE///////////////////////////////
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
+axios.get("https://api.github.com/users/bobbygondola").then((response) => {
+  let object = response;
+  // cardMaker(object);
+  parentDiv
+    .append(cardMaker(object))
+
+    .catch((error) => {
+      console.log(error);
+    });
+});
+const parentDiv = document.querySelector(".cards");
+// parentDiv.appendChild(".card")
+
+/* Step 2: Inspect and study the data coming back, this is YOUR  ///////////////////////////////////////DONE///////////////////////////////
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
-   Skip to Step 3.
+   Skip to Step 3./////////////////////////////////////////////////////////////////////////////////////DONE///////////////////////////////
 */
+
+const followersArray = [
+  "sadamexx",
+  "tetondan",
+  "dustinmyers",
+  "bigknell",
+  "justsml",
+];
+
+followersArray.forEach((name) => {
+  axios
+    .get(`https://api.github.com/users/${name}`)
+    .then((response) => {
+      parentDiv.appendChild(cardMaker(response));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
@@ -24,12 +56,12 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+//foreach thru after apend to parent div
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
-<div class="card">
+<div class="card"> ++++++++++++++++++++++++++++++++
   <img src={image url of user} />
   <div class="card-info">
     <h3 class="name">{users name}</h3>
@@ -53,3 +85,41 @@ const followersArray = [];
   luishrd
   bigknell
 */
+function cardMaker(object) {
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  userName.classList.add("username");
+
+  name.textContent = "Name: " + object.data.name;
+  userName.textContent = "Username: " + object.data.login;
+  location.textContent = "Location: " + object.data.location;
+  profile.textContent = "GitHub: " + object.data.url;
+  followers.textContent = "Followers: " + object.data.followers;
+  following.textContent = "Following: " + object.data.following;
+  bio.textContent = object.data.bio;
+  image.src = object.data.avatar_url;
+
+  return card;
+}
